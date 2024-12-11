ea1cad0eae1353be4209e8ee9821b2443080c015
congnguyen2222 <congnt.qig@gmail.com>
2024-12-11 14:51:29 +0700

    a

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ $(document).ready(function () {
   // Kích hoạt tooltip
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+// Khởi tạo CKEditor trên textarea
+CKEDITOR.replace("note", {
+  height: 150, // Chiều cao editor
+  toolbar: [
+    ["Bold", "Italic", "Underline", "Strike", "-", "RemoveFormat"],
+    ["NumberedList", "BulletedList", "-", "Outdent", "Indent"],
+    ["Link", "Unlink"],
+    ["Table", "HorizontalRule", "SpecialChar"],
+    ["Maximize", "Source"],
+  ],
+});
+
+ClassicEditor.create(document.querySelector("#editor"), {
+  versionCheck: false,
+});
